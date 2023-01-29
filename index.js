5becefc4c0a96fd77439ffb4c7d4b2dc9230557c
shwetha <shwethaj1104@gmail.com>
2023-01-29 13:39:02 +0530

    code clean up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const logger = require('./middleware/logger');
 
@@ -12,6 +13,10 @@ app.use(logger)
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+//handle bars middleware
+// app.engine('handlebars', exphbs({defaultLayout:'main'}));
+// app.set('view engine', 'handlebars');
 
 //using routes for cleaning up and reusing the routes//api routes
 app.use('/api/members',require('./routes/api/members'))
